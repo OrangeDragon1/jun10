3d26117454b6c21ea6b79b9222ae845917a5e896
OrangeDragon1 <wcks095@gmail.com>
2022-06-11 02:38:21 +0800

    flour pack solved

--- controlflow/core/FlourPacker.java
+++ controlflow/core/FlourPacker.java
@@ -3,37 +3,41 @@ package controlflow.core;
 public class FlourPacker {
 
     public static void main(String[] args) {
-        
-        System.out.println(canPack(0,5,4));
+
+        System.out.println(canPack(1, 0, 4));
     }
-    
+
     public static boolean canPack(int bigCount, int smallCount, int goal) {
-        
+
         boolean isPack = false;
 
-        if(bigCount >= 0 && smallCount >= 0 && goal >= 0) {
-            if(((bigCount * 5) + smallCount) >= goal) {
-                while(goal > 0) {
-                    while(bigCount != 0) {
+        if (bigCount >= 0 && smallCount >= 0 && goal >= 0) {
+            if (((bigCount * 5) + smallCount) >= goal) {
+                while (goal > 0) {
+                    while (bigCount != 0) {
                         bigCount--;
-                        goal -= 5;
-                        if(goal <= 0) 
+                        if ((goal -5) >= 0)
+                            goal -= 5;
+                        if (goal <= 0)
                             break;
                     }
-
-                    while(smallCount != 0) {
-                        smallCount--;
-                        goal -= 1;
-                        if(goal <= 0) 
-                            break;
+                    if (goal != 0) {
+                        while (smallCount != 0) {
+                            smallCount--;
+                            goal -= 1;
+                            if (goal <= 0)
+                                break;
+                        }
                     }
+                    if ((bigCount == 0) || (smallCount == 0))
+                        break;
                 }
-                if(goal == 0) {
+                if (goal == 0) {
                     isPack = true;
                 }
             }
         }
-        
+
         return isPack;
     }
 }
